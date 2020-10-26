7e6aac5bcc835f2df96bda094bde0e333d4505a2
Jérôme Gardou <jerome.gardou@reactos.org>
2020-10-26 09:04:49 +0100

    [NTOSKRNL] Store File Object in the segment instead of Section object

--- ntoskrnl/mm/ARM3/section.c
+++ ntoskrnl/mm/ARM3/section.c
@@ -1642,7 +1642,7 @@ MiGetFileObjectForSectionAddress(
         {
             /* Get the section pointer to the SECTION_OBJECT */
             Section = MemoryArea->SectionData.Section;
-            *FileObject = Section->FileObject;
+            *FileObject = ((PMM_SECTION_SEGMENT)Section->Segment)->FileObject;
         }
         else
         {
@@ -1685,7 +1685,7 @@ PFILE_OBJECT
 NTAPI
 MmGetFileObjectForSection(IN PVOID SectionObject)
 {
-    PSECTION Section;
+    PSECTION Section = SectionObject;
     ASSERT(KeGetCurrentIrql() == PASSIVE_LEVEL);
     ASSERT(SectionObject != NULL);
 
@@ -1693,12 +1693,11 @@ MmGetFileObjectForSection(IN PVOID SectionObject)
     if (MiIsRosSectionObject(SectionObject) == FALSE)
     {
         /* Return the file pointer stored in the control area */
-        Section = SectionObject;
         return Section->Segment->ControlArea->FilePointer;
     }
 
     /* Return the file object */
-    return ((PROS_SECTION_OBJECT)SectionObject)->FileObject;
+    return ((PMM_SECTION_SEGMENT)Section->Segment)->FileObject;
 }
 
 static
@@ -1720,7 +1719,7 @@ MiGetFileObjectForVad(
         {
             /* Get the section pointer to the SECTION_OBJECT */
             Section = MemoryArea->SectionData.Section;
-            FileObject = Section->FileObject;
+            FileObject = ((PMM_SECTION_SEGMENT)Section->Segment)->FileObject;
         }
         else
         {
